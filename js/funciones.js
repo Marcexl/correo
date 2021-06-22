74304ee5d467d74abc426baa6c900f122d78e77e
marcexl <mxlgallardo@gmail.com>
2021-06-22 00:28:30 -0300

    implementacion reglas para los inputs

--- js/funciones.js
+++ js/funciones.js
@@ -3,6 +3,142 @@
  * version 18062021
  */
 
+
+/* algunas reglas de texto */
+$('#username').on('input', function() {
+  var c = this.selectionStart,
+      r = /[^a-z0-9]/gi,
+      v = $(this).val();
+
+  if(r.test(v))
+  {
+    $(this).val(v.replace(r, ''));
+    c--;
+  }
+  this.setSelectionRange(c, c);
+});
+
+$('#userfname').on('input', function() {
+  var c = this.selectionStart,
+      r = /[^a-z \ \ñ \u00E0-\u00FC]/gi,
+      v = $(this).val();
+
+  if(r.test(v))
+  {
+    $(this).val(v.replace(r, ''));
+    c--;
+  }
+  this.setSelectionRange(c, c);
+});
+
+
+$('#userlname').on('input', function() {
+  var c = this.selectionStart,
+      r = /[^a-z \ \ñ \u00E0-\u00FC]/gi,
+      v = $(this).val();
+
+  if(r.test(v))
+  {
+    $(this).val(v.replace(r, ''));
+    c--;
+  }
+  this.setSelectionRange(c, c);
+});
+
+$('#useredad').on('input', function() {
+  var c = this.selectionStart,
+      r = /[^0-9]/gi,
+      v = $(this).val();
+
+  if(r.test(v))
+  {
+    $(this).val(v.replace(r, ''));
+    c--;
+  }
+  this.setSelectionRange(c, c);
+});
+
+$('#cuenta-fname').on('input', function() {
+  var c = this.selectionStart,
+      r = /[^a-z \ \ñ \u00E0-\u00FC]/gi,
+      v = $(this).val();
+
+  if(r.test(v))
+  {
+    $(this).val(v.replace(r, ''));
+    c--;
+  }
+  this.setSelectionRange(c, c);
+});
+
+$('#cuenta-lname').on('input', function() {
+  var c = this.selectionStart,
+      r = /[^a-z \ \ñ \u00E0-\u00FC]/gi,
+      v = $(this).val();
+
+  if(r.test(v))
+  {
+    $(this).val(v.replace(r, ''));
+    c--;
+  }
+  this.setSelectionRange(c, c);
+});
+
+$('#cuenta-user').on('input', function() {
+  var c = this.selectionStart,
+      r = /[^a-z \ \ñ \u00E0-\u00FC]/gi,
+      v = $(this).val();
+
+  if(r.test(v))
+  {
+    $(this).val(v.replace(r, ''));
+    c--;
+  }
+  this.setSelectionRange(c, c);
+});
+
+$('#cuenta-edad').on('input', function() {
+  var c = this.selectionStart,
+      r = /[^0-9]/gi,
+      v = $(this).val();
+
+  if(r.test(v))
+  {
+    $(this).val(v.replace(r, ''));
+    c--;
+  }
+  this.setSelectionRange(c, c);
+});
+
+
+$('#lugar').on('input', function() {
+  var c = this.selectionStart,
+      r = /[^a-z \ \ñ \u00E0-\u00FC]/gi,
+      v = $(this).val();
+
+  if(r.test(v))
+  {
+    $(this).val(v.replace(r, ''));
+    c--;
+  }
+  this.setSelectionRange(c, c);
+});
+
+$('#lugar-update').on('input', function() {
+  var c = this.selectionStart,
+      r = /[^a-z \ \ñ \u00E0-\u00FC]/gi,
+      v = $(this).val();
+
+  if(r.test(v))
+  {
+    $(this).val(v.replace(r, ''));
+    c--;
+  }
+  this.setSelectionRange(c, c);
+});
+
+
+
 function checkerror(code){
   let error = code;
   if(error == 403)
